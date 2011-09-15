0ff117e8eec67a56b1a0c8d74420d2ad36eedcae
Chisel <chisel@chizography.net>
2011-09-15 11:19:01 +0100

    Post-minting Initial Commit

---
+++ lib/Catalyst/Plugin/Session/Store/PSGI.pm
@@ -0,0 +1,8 @@
+package Catalyst::Plugin::Session::Store::PSGI;
+# ABSTRACT: Catalyst::Plugin::Session::Store::PSGI needs a more meaningful abstract
+use strict;
+use warnings;
+
+1;
+__END__
+# vim: ts=8 sts=4 et sw=4 sr sta
